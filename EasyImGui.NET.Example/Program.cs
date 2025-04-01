23948eb27506d79e48071fcc1dabaf61e3b064d2
Jiiks <stukov.alexei.va@gmail.com>
2025-04-01 08:59:55 +0300

    ImageProcessor

--- EasyImGui.NET.Example/Program.cs
+++ EasyImGui.NET.Example/Program.cs
@@ -58,6 +58,7 @@ internal class TestWindow(GameWindowSettings? s = null, NativeWindowSettings? n 
     // Define a texture that's in resx
     // Processed means the texture has already been processed and doesn't require any processing.
     // Eg it's already been ran through StbImage and saved as a raw binary.
+    // Use the included ImageProcessor to process any images if you'd like.
     [TextureDef(ResourceName = "testimg", Width = 123, Height = 128, Processed = true)]
     public uint TestTexture;
 
---
+++ EasyImGui.NET.ImageProcessor/Program.cs
@@ -0,0 +1,72 @@
+﻿using Hexa.NET.StbImage;
+
+namespace EasyImGui.NET.ImageProcessor;
+
+public unsafe class ImageProcessor {
+
+    public static void Main(string[] args) {
+        if (args.Length <= 0) {
+            Console.WriteLine("No directory supplied");
+            Console.ReadKey();
+            return;
+        }
+        var wd = args[0];
+
+        if (!Directory.Exists(wd)) {
+            Console.WriteLine($"Directory does not exist: {wd}");
+            Console.ReadKey();
+            return;
+        }
+
+        Console.WriteLine($"Working Directory: {wd}");
+
+        var files = Directory.GetFiles(wd);
+        if (files.Length <= 0) {
+            Console.WriteLine($"No files in {wd}");
+            Console.ReadKey();
+            return;
+        }
+
+        var parentDir = Directory.GetParent(wd);
+        if(parentDir == null) {
+            Console.WriteLine($"Parent dir does not exist");
+            Console.ReadKey();
+            return;
+        }
+
+        var dirName = new DirectoryInfo(wd).Name;
+
+        var outDir = Path.Join(parentDir.FullName, $"{dirName}_processed");
+        Console.WriteLine($"Out dir: {outDir}");
+
+        if (!Directory.Exists(outDir)) {
+            try {
+                Directory.CreateDirectory(outDir);
+            } catch (Exception e) {
+                Console.WriteLine(e.ToString());
+                Console.ReadKey();
+                return;
+            }
+        }
+
+        int x, y, channelsInFile = 0;
+
+        foreach (var file in files) {
+
+            var fn = Path.GetFileNameWithoutExtension(file);
+            var outPath = Path.Join(outDir, fn);
+            Console.WriteLine($"Processing {file} => {outPath}");
+
+            try {
+                var imagePtr = StbImage.Load(file, &x, &y, ref channelsInFile, 4);
+                var imageData = new Span<byte>(imagePtr, x * y * channelsInFile);
+
+                File.WriteAllBytes(outPath, imageData.ToArray());
+            } catch (Exception e) { Console.WriteLine(e.Message); }
+        }
+        
+        Console.WriteLine("All Done! Press any key to exit...");
+        Console.ReadKey();
+    }
+
+}
